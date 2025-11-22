7168bd24af701a97221672f1842e7538945d53eb
Davi Ricardo <davi.ricardo@csptech.com.br>
2025-11-22 14:39:13 -0300

    first commit

---
+++ frontend/src/services/recommendation.service.js
@@ -0,0 +1,14 @@
+// getRecommendations.js
+
+const baseURL = 'http://localhost:3001';
+
+const getRecommendations = (
+  formData = { selectedPreferences: [], selectedFeatures: [] },
+  products
+) => {
+  /**
+   * Crie aqui a lógica para retornar os produtos recomendados.
+   */
+};
+
+export default { getRecommendations };
